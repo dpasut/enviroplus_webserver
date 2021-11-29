d3c1b3d444a52f57ee3cbdfaa4434050706246a1
dpasut <daniel.j.pasut@gmail.com>
2021-11-29 10:18:31 -0500

    Black+iSort, add ltr559 fix

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,9 +1,17 @@
-from flask import Flask
 import time
-import ltr559
-from enviroplus import gas
-from bme280 import BME280
 from subprocess import PIPE, Popen
+
+try:
+    # Transitional fix for breaking change in LTR559
+    from ltr559 import LTR559
+
+    ltr559 = LTR559()
+except ImportError:
+    import ltr559
+
+from bme280 import BME280
+from enviroplus import gas
+from flask import Flask
 
 try:
     from smbus2 import SMBus
@@ -13,35 +21,42 @@ except ImportError:
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
+
 def getGas():
     readings = gas.read_all()
-    return(readings)
+    return readings
+
 
 def getCpuTemp():
-    process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
+    process = Popen(["vcgencmd", "measure_temp"], stdout=PIPE)
     output, _error = process.communicate()
     output = output.decode()
-    return float(output[output.index('=') + 1:output.rindex("'")])
+    return float(output[output.index("=") + 1 : output.rindex("'")])
+
 
 def getTemp():
-    #adjust factor for better results, higher factor to increse temp
+    # adjust factor for better results, higher factor to increse temp
     factor = 0.8
     rawTemp = bme280.get_temperature()
     cpuTemp = getCpuTemp()
-    calcTemp = cpuTemp - (cpuTemp-rawTemp)/factor
-    return(calcTemp)
+    calcTemp = cpuTemp - (cpuTemp - rawTemp) / factor
+    return calcTemp
+
 
 def getPressure():
-    return(bme280.get_pressure())
+    return bme280.get_pressure()
+
 
 def getHumidity():
-    return(bme280.get_humidity())
+    return bme280.get_humidity()
+
 
 def getLight():
-    return(ltr559.get_lux())
+    return ltr559.get_lux()
 
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def main():
@@ -50,7 +65,7 @@ def main():
     pressure = getPressure()
     light = getLight()
     humidity = getHumidity()
-    return ("""
+    return """
             <meta http-equiv='refresh' content='1'>
             Welcome to the EnviroPi Webserver!<br/>
             Temperature = {} <br/>
@@ -58,14 +73,20 @@ def main():
             Pressure = {} <br/>
             Light = {} <br/>
             Humidity = {} <br/>
-        """.format(temp, gas, pressure, light, humidity))
+        """.format(
+        temp, gas, pressure, light, humidity
+    )
+
+
 @app.route("/debug")
 def debug():
     cpuTemp = getCpuTemp()
     rawTemp = bme280.get_temperature()
     temp = getTemp()
-    return("""
+    return """
     CPU Temp = {} <br/>
     Raw Temp = {} <br/>
     Temp = {} <br/>
-    """.format(cpuTemp, rawTemp, temp))
+    """.format(
+        cpuTemp, rawTemp, temp
+    )
